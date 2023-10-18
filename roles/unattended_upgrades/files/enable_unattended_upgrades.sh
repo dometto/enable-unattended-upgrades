711cfe304d6d2ec8037347d4b3e98c7b5e7b296b
Dawa Ometto <d.l.a.ometto1@uu.nl>
2023-10-18 16:59:21 +0200

    Shellcheck

--- roles/unattended_upgrades/files/enable_unattended_upgrades.sh
+++ roles/unattended_upgrades/files/enable_unattended_upgrades.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -e
 LOG=/var/log/post-cloud-init.log
-# Block until cloud-init completes
+
 echo "Running cloud-init status --wait..." >> $LOG
+# Block until cloud-init completes
 cloud-init status --wait > /dev/null 2>&1
 [ $? -ne 0 ] && echo 'Cloud-init failed' >> $LOG && exit 1
-echo 'Cloud-init succeeded at ' `date -R` >> $LOG
+echo 'Cloud-init succeeded at ' $(date -R) >> $LOG
 
 systemctl enable apt-daily.timer || echo "Could not enable apt-daily.timer" >> $LOG
 systemctl enable apt-daily-upgrade.timer || echo  "Could not enable apt-daily-upgrade.timer" >> $LOG

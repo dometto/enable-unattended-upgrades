971c1e76f3f36a96be66551b47cc345f82dbe0ad
Dawa Ometto <d.l.a.ometto1@uu.nl>
2023-10-19 11:58:04 +0200

    Delay enabling timer by 1h

--- roles/unattended_upgrades/files/enable_unattended_upgrades.sh
+++ roles/unattended_upgrades/files/enable_unattended_upgrades.sh
@@ -7,6 +7,9 @@ cloud-init status --wait > /dev/null 2>&1
 [ $? -ne 0 ] && echo 'Cloud-init failed' >> $LOG && exit 1
 echo 'Cloud-init succeeded at ' $(date -R) >> $LOG
 
+# Run our commands one hour after we detect that cloud-init is done
+sleep 3600
+
 systemctl enable apt-daily.timer || echo "Could not enable apt-daily.timer" >> $LOG
 systemctl enable apt-daily-upgrade.timer || echo  "Could not enable apt-daily-upgrade.timer" >> $LOG
 systemctl start apt-daily.timer || echo  "Could not start apt-daily.timer" >> $LOG

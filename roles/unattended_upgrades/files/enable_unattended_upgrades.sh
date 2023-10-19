d09e0f03bc3e78ff71530a00d032a3751dfd4ae4
Dawa Ometto <d.l.a.ometto1@uu.nl>
2023-10-19 12:03:00 +0200

    Add debugging to script

--- roles/unattended_upgrades/files/enable_unattended_upgrades.sh
+++ roles/unattended_upgrades/files/enable_unattended_upgrades.sh
@@ -5,10 +5,11 @@ echo "$(date -R): Running cloud-init status --wait..." >> $LOG
 # Block until cloud-init completes
 cloud-init status --wait > /dev/null 2>&1
 [ $? -ne 0 ] && echo 'Cloud-init failed' >> $LOG && exit 1
-echo 'Cloud-init succeeded at ' $(date -R) >> $LOG
+echo "Cloud-init succeeded at $(date -R)" >> $LOG
 
 # Run our commands one hour after we detect that cloud-init is done
 sleep 3600
+echo "Enabling unattended upgrade timers at $(date -R)" >> $LOG
 
 systemctl enable apt-daily.timer || echo "Could not enable apt-daily.timer" >> $LOG
 systemctl enable apt-daily-upgrade.timer || echo  "Could not enable apt-daily-upgrade.timer" >> $LOG
